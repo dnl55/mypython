48859a6e0d79fad3e4ff610c8df2746322dbdbcb
dnl55 <dnl16@mail.ru>
2022-01-25 22:35:06 +0500

    3 урок
    добавил сообщение при написании /start и добавил туда форматирование для вывода имени того кто написал старт боту, попробовать поиграть и добавить вывод не имени а ссылки на профиль с помащью поиска данного атрибута в словаре через print(updater)

--- python-telegram-bot 01/onebot.py
+++ python-telegram-bot 01/onebot.py
@@ -1,7 +1,10 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-def start_bot(bot, updater):
-    print("Hello")
+def start_bot(update: Updater, context: CallbackContext):
+    mytext = ("""Привет {}.
+    Напиши или нажми /start чтоб я снова вывел это сообщение,
+    возможно в дальнейшем тут будет что та по полезнее)""".format(update.message.chat.first_name) )
+    update.message.reply_text(mytext)
 
 def main():
     updtr = Updater("1985672039:AAF-uV6oITqeCiLR7ADTlxBlALYROTTuKCg")
