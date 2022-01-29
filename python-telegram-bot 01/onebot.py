74c99681ed5c43db35379d779d9dbd4a55f731de
dnl55 <dnl16@mail.ru>
2022-01-29 21:05:54 +0500

    5 урок
    добавился ответ на сообщение, теперь бот реагирует на сообщение ответом Hello

--- python-telegram-bot 01/onebot.py
+++ python-telegram-bot 01/onebot.py
@@ -1,20 +1,31 @@
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import logging
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, filename='bot.log')
 
 def start_bot(update: Updater, context: CallbackContext):
+    print(update)
     mytext = ("""Привет {}.
     Напиши или нажми /start чтоб я снова вывел это сообщение,
-    возможно в дальнейшем тут будет что та по полезнее)""".format(update.message.chat.first_name) )
+    возможно в дальнейшем тут будет что та по полезнее)""".format(update.message.chat.username) )
     update.message.reply_text(mytext)
+#Функция где прописано что будет выводиться в чат 
+def chat (update: Updater, context: CallbackContext):
+    text = 'Hello'
+    update.message.reply_text(text)
 
 def main():
     updtr = Updater("1985672039:AAF-uV6oITqeCiLR7ADTlxBlALYROTTuKCg")
 
     updtr.dispatcher.add_handler(CommandHandler("start", start_bot))
+    #выводит сообщение когда что та пишешь в чат
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
 
     updtr.start_polling()
     updtr.idle()
 
 
 if __name__ == "__main__":
+    logging.info('Bot started!')
     main()
      
